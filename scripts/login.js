dd3a1a48e81101c8d1b8ffeeb50945174c954573
Md Abdul Wazed <91973093+wazed-md-abdul@users.noreply.github.com>
2026-03-08 07:26:42 +0800

    Change redirect path to home.html

--- scripts/login.js
+++ scripts/login.js
@@ -3,7 +3,7 @@ const loginFunction = () => {
     const password = document.querySelector("#password").value;
     if (username === "admin" && password === "admin123") {
         alert("Login successful!");
-        window.location.assign("./home.html");
+        window.location.assign("/home.html");
     }
      else {
         alert("Invalid username or password. Please try again.");
